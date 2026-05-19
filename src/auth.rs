9eb07db77a8bbdca542673792ebcda5ea8f9f091
eduardo.llerandi1 <eduardo.llerandi1@volkswagen-groupservices.com>
2026-05-19 19:02:02 +0200

    feat: searching for authorization header and cast bytes to string

--- src/auth.rs
+++ src/auth.rs
@@ -13,4 +13,11 @@ where
 {
     type Rejection = (StatusCode, &'static str);
 
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        let auth_header = parts
+            .headers
+            .get("Authorization")
+            .and_then(|value| value.to_str().ok());
+
+    }
 }

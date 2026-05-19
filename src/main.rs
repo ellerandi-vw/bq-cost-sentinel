d15aebe18134eac9f972da24237d5296e82fded6
eduardo.llerandi1 <eduardo.llerandi1@volkswagen-groupservices.com>
2026-05-19 15:05:39 +0200

    feat: logs formatter

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,11 @@ use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
 async fn main() {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .json()
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Error initializing the logging (tracing) component");
 
 }

38f0203c6c9c4905fe6bcedec37a81099b87d59d
eduardo.llerandi1 <eduardo.llerandi1@volkswagen-groupservices.com>
2026-05-19 19:19:25 +0200

    feat: response implementation looking for "query" value in the JSON tree

--- src/server.rs
+++ src/server.rs
@@ -20,4 +20,23 @@ pub async fn proxy_query(
     Json(payload): Json<Value>, 
 ) -> impl IntoResponse {
     
+    info!(
+        project_id = %project_id,
+        "Request for a query sent to BigQuery was intercepted"
+    );
+
+    let sql_query = payload
+        .get("query")
+        .and_then(|q| q.as_str())
+        .unwrap_or("Query not found in the payload");
+
+    warn!(
+        query_text = %sql_query,
+        "Simulating evaluation (not yet connected to Google Cloud)"
+    );
+
+    (
+        StatusCode::OK,
+        format!("Simulation OK. Request for project {} intercepted", project_id),
+    )
 }

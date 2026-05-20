15503aaaa8f6f47e52afb7d71dd02105e90a6234
eduardo.llerandi1 <eduardo.llerandi1@volkswagen-groupservices.com>
2026-05-20 11:10:06 +0200

    feat: struct for bq response; google uses camelCase but Rust snake_case

--- src/google_client.rs
+++ src/google_client.rs
@@ -2,3 +2,8 @@ use reqwest::{Client, StatusCode};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct BqDryRunResponse {
+    pub total_bytes_processed: Option<String>, 
+}
